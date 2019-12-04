b4c4b9f77a80d81d039fd95fb0030cda3e40d36e
Iqra Masroor <masrooriqra@gmail.com>
2019-12-04 22:58:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,6 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  fruit = {
-    hash: key
-  }
-  fruit[:new_key] = key
+  hash[:new_key] = key
+  hash[key] += 1
 end
